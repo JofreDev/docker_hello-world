0aff93d9e5647d76ccb73e3e4ac937be31f62401
JofreDev <gavidiajofreeduardo@gmail.com>
2024-07-25 11:23:08 -0500

    project

--- backend/tests/index.test.js
+++ backend/tests/index.test.js
@@ -23,4 +23,9 @@ describe('Prueba de otros tests', () => {
     it('prueba de otra cosa!!!!', () => {
         assert.equal(3 - 4, -1, "No son iguales")
     })
+
+
+    it('prueba de otra cosaaaaaa', () => {
+        assert.equal(3 - 4, -1, "No son iguales")
+    })
 })
